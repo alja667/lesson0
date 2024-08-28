0246aced682669d9e2f5a6b8fe88a0b62b8b552c
alja667 <aprogozhina@gmail.com>
2024-08-28 21:42:18 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,12 @@
+##1st program
+print(9**0.5*5)
+#2st program
+print(9.99>9.98 and 1000 !=1000.1)
+#3st program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 ==2*(2+2))
+#4st program
+y=float(123.456)
+print(y*10)
+print(int(y//25))
